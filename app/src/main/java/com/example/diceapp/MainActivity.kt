a433c406b6c874a8c3943161384c5e1be19e3168
Adeel <71775487+ade3l@users.noreply.github.com>
2021-10-24 10:40:14 +0300

    added code for random roll

--- app/src/main/java/com/example/diceapp/MainActivity.kt
+++ app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.diceapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        val resultTextView : TextView = findViewById(R.id.textView)
+        val dice=Dice(6)
+        rollButton.setOnClickListener{
+            resultTextView.setText(dice.roll().toString())
+        }
+    }
+
+    class Dice(numSide:Int) {
+        val side=numSide
+        fun roll():Int{
+            return ((1..side).random())
+        }
     }
 }
